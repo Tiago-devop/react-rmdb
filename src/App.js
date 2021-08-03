cd4552f5b656f790ae1f14735926647ce471a8ac
Tiagodev <tiagodev@protonmail.com>
2021-08-03 11:44:36 -0700

    add Header comp.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Header from "./components/Header";
+import { GlobalStyle } from "./GlobalStyle";
+
 const App = () => {
-  return <div className="App">Start here.</div>;
+  return (
+    <div className="App">
+      <Header />
+      Start here.
+      <GlobalStyle />
+    </div>
+  );
 };
 
 export default App;
