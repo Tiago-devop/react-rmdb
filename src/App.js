e0436b0b80aa26cb6150bb1ee1a31512ec75e07b
Tiagodev <tiagodev@protonmail.com>
2021-08-03 12:34:26 -0700

    adding Home comp.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import Home from "./components/Home";
 import { GlobalStyle } from "./GlobalStyle";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      Start here.
+      <Home />
       <GlobalStyle />
     </div>
   );
