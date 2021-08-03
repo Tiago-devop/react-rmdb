3a4a9210a18034d643372d178a2b309fe7ff5f3e
Tiagodev <tiagodev@protonmail.com>
2021-08-03 13:11:47 -0700

    fetching data from the mdb api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import API from "../API";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 import NoImage from "../images/no_image.jpg";
@@ -8,11 +10,31 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  return (
-    <div>
-      Home Page      
-    </div>
-  )
-}
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
 
-export default Home
+      const movies = await API.fetchMovies(searchTerm, page);
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }));
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  console.log(state);
+
+  return <div>Home Page</div>;
+};
+
+export default Home;
