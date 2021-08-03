1c81ffa3c1291d2139cce58d468a574d0b69758f
Tiagodev <tiagodev@protonmail.com>
2021-08-03 11:14:32 -0700

    adding global styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-import React from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      Start here.
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">Start here.</div>;
+};
 
 export default App;
