42cf399a722b6940244c7d7097c5e37fd1188a27
Tiagodev <tiagodev@protonmail.com>
2021-08-04 11:23:55 -0700

    add HeroImage comp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,30 @@
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
-import NoImage from "../images/no_image.jpg";
 // Components
+import HeroImage from "./HeroImage";
 
 // Hook
 import { useHomeFetch } from "../hooks/useHomeFetch";
+
+// Image
+import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
   const { state, loading, error } = useHomeFetch();
   console.log(state);
 
-  return <div>Home Page</div>;
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  );
 };
 
 export default Home;
