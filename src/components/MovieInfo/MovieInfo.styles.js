572dae5c3445023e19a737262d3d8c3a8879aea8
Tiagodev <tiagodev@protonmail.com>
2021-08-09 11:39:47 -0700

    add movieInfo comp.

---
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { IMAGE_BASE_URL, BACKDROP_SIZE } from "../../config";
+
+export const Wrapper = styled.div``;
+
+export const Content = styled.div``;
+
+export const Text = styled.div``;
+
+export const RatingDirectors = styled.div``;
+
+export const Score = styled.div``;
+
+export const Director = styled.div``;
