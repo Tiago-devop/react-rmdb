2d8d45c9989ad8ff207abec3565d0ad53eb3fdaa
Tiagodev <tiagodev@protonmail.com>
2021-08-09 14:38:57 -0700

    add styling movieInfoBar

--- src/components/MovieInfoBar/MovieInfoBar.styles.js
+++ src/components/MovieInfoBar/MovieInfoBar.styles.js
@@ -1,7 +1,42 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  min-height: 100px;
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  max-width: var(--maxWith);
+  width: 100%;
+  margin: 0 auto;
 
-export const Column = styled.div``;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--medGrey);
+  border-radius: 20px;
+  margin: 0 20px;
+  flex: 1;
+
+  @media screen and (max-width: 768px) {
+    margin: 20px;
+
+    :first-child {
+      margin-bottom: 0;
+    }
+
+    :last-child {
+      margin-top: 0;
+    }
+  }
+`;
